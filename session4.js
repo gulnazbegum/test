3c49924b7e7b4a3658b7e2139c01d33d052f870a
MumtazBegum <47818324+gulnazbegum@users.noreply.github.com>
2023-08-08 23:42:09 +0530

    Add files via upload

---
+++ session4.js
@@ -0,0 +1,54 @@
+// let age = 20;
+
+// let name = 'john'
+
+// futurejob = 'web Developer'
+
+// let myIntro = 'My name is'+ name + 'I am' + age +
+// 'years old and I will soon become a ' + futurejob;
+
+// console.log(myIntro)
+
+// let myIntro2 = `My name is ${name} I am ${age} years old 
+// and I will soon become a ${ futurejob}`
+
+// console.log(myIntro2)
+
+//let age = 20;
+
+// let age = prompt('Please enter your age: ')
+// if(age > 17) {
+//     alert('You can drive in the UK')
+// }else if(age<17) {
+//     alert('sorry, you can not dive in the UK')
+// }else { 
+//     alert('No Info found')
+// }
+
+
+// function add2Numbers(a, b){
+//     let c = a -b;
+
+//     return c
+// }
+
+// let result = add2Numbers(18,33)
+
+// console.log(result)
+
+
+// let girl ={
+//     name='test',
+//     age ='30'
+//     greeting : function{
+//         alert('Hi jane')
+//     },
+//     boyfriend =''
+// }
+
+// console.log(girl.name)
+
+
+function contactbtn() {
+    alert("Who!!  y ou have discovred")
+}
